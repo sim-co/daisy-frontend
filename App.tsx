b0f062b75b2ccecd048eed539d0d9677a70cc67e
BHyeonKim <rlaqhguse@gmail.com>
2023-02-01 00:26:47 +0900

    feat: feature: redirect to mainscreen when user login 유저가 로그인하면 엑세스토큰과 리프레쉬토큰을 리덕스와 asyncStorage에 저장

--- App.tsx
+++ App.tsx
@@ -4,30 +4,46 @@ import React from 'react';
 import Config from 'react-native-config';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
+import {useAppSelector} from './src/redux/hooks';
 import LoginScreen from './src/screen/LoginScreen';
+import MainScreen from './src/screen/MainScreen';
 import RegisterScreen from './src/screen/RegisterScreen';
 import WebViewScreen from './src/screen/WebViewScreen';
 import StorybookUI from './storybook';
 
-export type RootStackParamList = {
+export type AuthStackParamList = {
   Login: undefined;
   WebView: {oauthProvider: 'naver' | 'kakao' | 'google'};
   Register: undefined;
 };
 
+export type UserStackParamList = {
+  Main: undefined;
+};
+
 const App = () => {
-  const Stack = createNativeStackNavigator<RootStackParamList>();
+  const UserStack = createNativeStackNavigator<UserStackParamList>();
+  const AuthStack = createNativeStackNavigator<AuthStackParamList>();
+  const {isAuthenticated} = useAppSelector(state => state.auth);
+
+  console.log('isAuthenticated', isAuthenticated);
 
   return (
     <NavigationContainer>
       <SafeAreaProvider>
-        <Stack.Navigator
-          initialRouteName="Login"
-          screenOptions={{headerShown: false}}>
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="WebView" component={WebViewScreen} />
-          <Stack.Screen name="Register" component={RegisterScreen} />
-        </Stack.Navigator>
+        {isAuthenticated ? (
+          <UserStack.Navigator initialRouteName="Main">
+            <UserStack.Screen name="Main" component={MainScreen} />
+          </UserStack.Navigator>
+        ) : (
+          <AuthStack.Navigator
+            initialRouteName="Login"
+            screenOptions={{headerShown: false}}>
+            <AuthStack.Screen name="Login" component={LoginScreen} />
+            <AuthStack.Screen name="WebView" component={WebViewScreen} />
+            <AuthStack.Screen name="Register" component={RegisterScreen} />
+          </AuthStack.Navigator>
+        )}
       </SafeAreaProvider>
     </NavigationContainer>
   );
--- src/screen/WebViewScreen/index.tsx
+++ src/screen/WebViewScreen/index.tsx
@@ -2,15 +2,20 @@ import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import {WebView} from 'react-native-webview';
+import {WebViewNavigation} from 'react-native-webview/lib/WebViewTypes';
 
-import {RootStackParamList} from '../../../App';
+import {AuthStackParamList} from '../../../App';
+import {login} from '../../redux/AuthSlice';
+import {useAppDispatch} from '../../redux/hooks';
+import {storeToken} from '../../util/asyncstorage';
 
 export type WebViewScreenProps = NativeStackScreenProps<
-  RootStackParamList,
+  AuthStackParamList,
   'WebView'
 >;
 
 const WebViewScreen = ({route}: WebViewScreenProps) => {
+  const dispatch = useAppDispatch();
   const {oauthProvider} = route.params;
   const oauthUri = {
     naver: process.env.OAUTH_NAVER_URI,
@@ -18,9 +23,24 @@ const WebViewScreen = ({route}: WebViewScreenProps) => {
     google: process.env.OAUTH_GOOGLE_URI,
   }[oauthProvider];
 
+  const handleNavigationStateChange = (naviagtionState: WebViewNavigation) => {
+    const url = naviagtionState.url;
+    if (!url.includes('access=')) {
+      return;
+    }
+    const [accessToken, refreshToken] = url
+      .split('access=')[1]
+      .split('refresh=');
+    storeToken({accessToken, refreshToken});
+    dispatch(login({accessToken, refreshToken}));
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <WebView source={{uri: `${oauthUri}`}} />
+      <WebView
+        source={{uri: `${oauthUri}`}}
+        onNavigationStateChange={handleNavigationStateChange}
+      />
     </SafeAreaView>
   );
 };
