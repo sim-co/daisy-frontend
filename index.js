f55df199cfedc0406fea18b579b6044159e16c2c
BHyeonKim <rlaqhguse@gmail.com>
2023-02-01 00:23:34 +0900

    fix: fix import path

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import {Provider} from 'react-redux';
 
 import App from './App';
 import {name as appName} from './app.json';
-import store from './src/redux/store';
+import {store} from './src/redux/store';
 
 const reduxApp = () => {
   return (
